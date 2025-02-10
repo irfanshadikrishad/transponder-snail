469dd101c08ac9e706a6627f9f2fbc2ee37249a2
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2025-02-10 23:22:17 +0600

    feat: nextjs-initialization

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "auto",
+  weight: "400",
+});
+
+export const metadata: Metadata = {
+  title: "Transponder Snail",
+  description:
+    "Chat with your friends and family without worrying about privacy.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className}`}>{children}</body>
+    </html>
+  );
+}
--- server/routes/userRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { allUsers } from "../controllers/userController.js";
-import authMiddleware from "../middlewares/authMiddleware.js";
-
-const router = Router();
-
-router.route("/").get(authMiddleware, allUsers);
-
-export default router;
